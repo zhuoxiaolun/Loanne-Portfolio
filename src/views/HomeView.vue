69356a2dadb7bf7a4c8518e47b7d448cebfabfcf
zhuoxiaolun <zhuoxl3729@gmail.com>
2026-07-12 20:33:21 +0800

    Revert "feat(home): add Tools by AI section above footer"
    This reverts commit cf0a3855b160c8265660c30dcc5a813d290627e0.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,12 @@
       <ProjectGrid :projects="featuredProjects" />
     </section>
 
-    <section class="section">
-      <AiToolsSection />
-    </section>
-
   </main>
 </template>
 
 <script setup lang="ts">
 import HeroSection from '@/components/organisms/HeroSection.vue'
 import ProjectGrid from '@/components/organisms/ProjectGrid.vue'
-import AiToolsSection from '@/components/organisms/AiToolsSection.vue'
 import homeData from '@/data/home.json'
 import type { Project } from '@/types'
 
